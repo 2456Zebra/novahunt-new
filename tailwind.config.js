8c679c7d55950372334177f530d11edc5bdcdb29
2456Zebra <2456Zebra@gmail.com>
2025-12-15 11:37:26 -0800

    Remove custom font family from Tailwind config
    Removed custom font family extension from Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
